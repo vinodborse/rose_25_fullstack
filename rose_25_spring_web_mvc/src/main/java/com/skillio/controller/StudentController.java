59d3432dff16e6bf68c1811777605d54245b979f
Vinodkumar Borse <borse002@gmail.com>
2026-04-02 09:27:20 +0530

    Add files via upload

--- rose_25_spring_web_mvc/src/main/java/com/skillio/controller/StudentController.java
+++ rose_25_spring_web_mvc/src/main/java/com/skillio/controller/StudentController.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,4 +48,41 @@ public class StudentController {
 		students.add(s3);
 		return students;
 	}
+	
+	/*@GetMapping("/std-rollno/{rollno}") // http://localhost:8080/rose_25_spring_web_mvc/std-rollno/4/Vinod
+	public Student getStudentByRollNo(@PathVariable("rollno") int rollno) {*/
+	
+	@GetMapping("/std-rollno") 	// http://localhost:8080/rose_25_spring_web_mvc/std-rollno?rn=3
+	public Student getStudentByRollNo(@RequestParam("rn") int rollno) {
+		
+		Student s1 = new Student(1, "Don", "Dhule");
+		Student s2 = new Student(2, "Salmon", "Mumbai");
+		Student s3 = new Student(3, "Aliya", "Delhi");
+		List<Student> students = new ArrayList<>();
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+
+		Student std = new Student(); 
+		
+		for(Student s : students) {
+			if(rollno == s.getRollno()) {
+				std = s;
+				break;
+			}
+		}
+			
+		return std;
+
+		
+	}
+	
+	@PostMapping("/add-student")
+	public String addStudent(@RequestBody Student s) {		
+		return "Student name is:- " + s.getName();
+	}
+	
+	
+	
+	
 }
